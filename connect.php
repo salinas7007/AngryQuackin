f5b350dfc1ff5009e6193c79e55de7b2a0f0e877
salinas7007 <53586305+salinas7007@users.noreply.github.com>
2020-12-11 22:44:12 -0600

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,8 +1,8 @@
 <?php
-$server = "localhost"; 
-$username   = "elizachick"; /*add in what needs to be added*/
-$password   = "59OaCYjpNE7v"; //what goes here is my stuff
-$database   = "elizachi_forum"; //for this local engine
+$server = "HOSTNAME"; 
+$username   = "USERNAME"; /*add in what needs to be added*/
+$password   = "PASSWORD"; //what goes here is my stuff
+$database   = "forum"; //for this local engine
  
  //Create connection
  $conn = mysqli_connect($server, $username, $password, $database);
